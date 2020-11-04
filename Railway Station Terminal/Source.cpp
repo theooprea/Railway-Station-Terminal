7fd77d5d224672cae92d5506e795cb7798226eeb
theooprea <opreatheodor54@gmail.com>
2020-11-04 23:11:12 +0200

    added POO concepts, changed everything to work with object oriented

--- Railway Station Terminal/Source.cpp
+++ Railway Station Terminal/Source.cpp
@@ -4,66 +4,132 @@
 
 using namespace std;
 
-class Station {
-	private:
-		string name;
-		int nr_railways;
-		vector<list<int>> railways;
-	public:
-		Station(int nr_railways, char *name) {
-			this->nr_railways = nr_railways;
-			this->name = name;
-			for (int i = 0; i < nr_railways; i++) {
-				railways.push_back(list<int>());
-			}
-		}
-		void print_station() {
-			cout << "-----" << name << "-----" << endl;
-			print_railways();
-		}
- 		void print_railways() {
-			for (int i = 0; i < nr_railways; i++) {
-				print_railway(i);
-			}
-		}
-		void print_railway(int railway) {
-			if (railway >= nr_railways) {
-				cout << "No such railway" << endl;
-			}
-			else {
-				int lines = 80 - 8 * railways[railway].size();
-				for (int i = 0; i < lines; i++) {
-					cout << "-";
-				}
-				for (int i : railways[railway]) {
-					cout << i << "----";
-				}
-				cout << endl;
-			}
-		}
-		void add_train(int train, int railway) {
-			if (railway >= nr_railways) {
-				cout << "The given railway doesn't exist!(" <<railway<<")"<< endl;
-			}
-			else {
-				if (railways[railway].size() == 6) {
-					cout << "Railway full, please select another one" << endl;
-				}
-				else {
-					railways[railway].push_front(train);
-				}
-			}
-		}
-};
-
-int digits(int number) {
+int digits(int n) {
 	int count = 0;
-	while (number > 0) {
+	while (n > 0) {
 		count++;
-		number /= 10;
+		n /= 10;
 	}
 	return count;
 }
+// TODO implement counting the number of - to add per railway
+/*int lines_per_railway(Railway railway) {
+	int counter = 0;
+	for ()
+}*/
+
+class Train {
+private:
+	int number;
+	string type;
+	string abreviation;
+public:
+	Train() {
+		number = 0;
+		type = "NotDefined";
+		abreviation = "ND";
+	}
+	Train(int number) {
+		this->number = number;
+		if (number >= 500 && number < 600) {
+			type = "InterCity";
+			abreviation = "IC";
+		}
+		else if (number >= 2000 && number < 10000) {
+			type = "Regio";
+			abreviation = "R";
+		}
+		else {
+			type = "InterRegio";
+			abreviation = "IR";
+		}
+	}
+	int getNumber() {
+		return number;
+	}
+	string getType() {
+		return type;
+	}
+	string getAbreviation() {
+		return abreviation;
+	}
+};
+class Railway {
+private:
+	list<Train> trains;
+	int railway_nr;
+public:
+	Railway(int railway_nr) {
+		this->railway_nr = railway_nr;
+	}
+	void add_train(Train train) {
+		trains.push_front(train);
+	}
+	void print_railway() {
+		cout << "Railway nr: " << railway_nr << " ";
+		int lines = 80 - 8 * trains.size();
+		for (int i = 0; i < lines; i++) {
+			cout << "-";
+		}
+		for (Train current : trains) {
+			cout << current.getNumber() << "---";
+		}
+		cout << endl;
+	}
+	int getSize() {
+		return trains.size();
+	}
+};
+class Station {
+private:
+	string name;
+	int nr_railways;
+	vector<Railway> railways;
+public:
+	Station(int nr_railways, char *name) {
+		this->nr_railways = nr_railways;
+		this->name = name;
+		for (int i = 0; i < nr_railways; i++) {
+			Railway new_railway(i);
+			railways.push_back(new_railway);
+		}
+	}
+	void print_station() {
+		cout << "-----" << name << "-----" << endl;
+		for (int i = 0; i < nr_railways; i++) {
+			railways[i].print_railway();
+		}
+
+	}
+	void print_railways() {
+		for (int i = 0; i < nr_railways; i++) {
+			railways[i].print_railway();
+		}
+	}
+	void print_railway(int railway_nr) {
+		railways[railway_nr].print_railway();
+	}
+	void add_train(int train_number, int railway_number) {
+		if (train_number >= 200 && train_number < 17000) {
+			if (railway_number >= nr_railways) {
+				cout << "The given railway doesn't exist!(" << railway_number << ")" << endl;
+			}
+			else {
+				if (railways[railway_number].getSize() == 8) {
+					cout << "Railway full, please select another one" << endl;
+				}
+				else {
+					Train new_train(train_number);
+					railways[railway_number].add_train(new_train);
+				}
+			}
+		}
+		else {
+			cout << "The given train doesn't exist according to CFR!(" << train_number << ")" << endl;
+		}
+		
+	}
+};
 
 int main() {
 	FILE *in_file = fopen("./config_file.txt", "r");
